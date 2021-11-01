75461121fae7d09e716d91d9eb1c4b559edfbcfc
João Mangueira <jamjs7@gmail.com>
2021-11-01 16:46:41 -0300

    procura do filme feita

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
 import './App.css';
 
 function App() {
 
     const [movieList, setMovielist] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
             setMovielist(list);
+
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].item.results.length - 1));
+            let chosen = originals[0].item.results[randomChosen];
+
+            let chosenInfo = await Tmdb.getMovieId(chosen.id, 'tv');
+            setFeaturedData(chosenInfo);
         }
 
         loadAll();
     }, []);
   return (
     <div className="page">
+        {featuredData &&
+            <FeatureMovie item={featuredData}/>
+        }
         <section className="list">
             {movieList.map((item, key) => (
                   <MovieRow key={key} title={item.title} items={item.item}/>
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -61,7 +61,27 @@ export default {
                 title: 'Documentários',
                 item: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
-
         ]
+    },
+    getMovieId: async (movieId, type) => {
+        let info = {
+
+        }
+
+        if(movieId) {
+            switch (type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                default:
+                    info = null;
+                    break;
+            }
+        }
+        return info;
     }
+
 }
