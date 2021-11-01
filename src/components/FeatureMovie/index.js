ac1cc543bac211fbe88e0419900b3647e200a3d9
João Mangueira <jamjs7@gmail.com>
2021-11-01 16:54:11 -0300

    efeito de transparencia na imagem do filme em destaque

--- src/components/FeatureMovie/index.js
+++ src/components/FeatureMovie/index.js
@@ -8,6 +8,9 @@ export default ({item}) => {
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original/${item.backdrop_path})`
         }}>
+            <div className='featured--vertical'>
+                <div className='featured--horizontal'></div>
+            </div>
             <div>{item.original_name}</div>
         </section>
     );
